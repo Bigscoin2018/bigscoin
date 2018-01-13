ff8422b40dc0914c12e412d7885b12a527d3fe1b
Bigscoin2018 <2018.bigscoin@gmail.com>
2018-01-13 09:49:59 +0700

    upload source bigscoin

---
+++ README.md
@@ -0,0 +1,5 @@
+rpc port: 32872
+net port: 32862
+
+
+BigsCoin.com
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BigsCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BigsCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
